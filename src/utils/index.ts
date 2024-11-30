5e9963afcaf07e7c3017a37cd2bc9f3cb097c037
Martin Doyle <github@martindoyle.com>
2024-11-30 06:27:21 +0000

    Clean start

--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './common-algorithms';
-export * from './file-utils';
-export * from './formatters';
-export * from './logging';
-export * from './math-utils';
-export * from './string-utils';
--- src/utils/string-utils.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable id-length */
-export const reverseString = (input: string): string => {
-  let reversed = '';
-  for (let i = input.length - 1; i >= 0; i--) {
-    reversed += input.charAt(i);
-  }
-  return reversed;
-};
