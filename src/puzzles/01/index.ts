b8ad2ab27b320eb98d3ff18fd673cf90329cbc8b
Martin Doyle <github@martindoyle.com>
2024-12-01 05:16:03 +0000

    Complete first day's puzzles

--- src/puzzles/01/index.ts
+++ src/puzzles/01/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
@@ -5,7 +7,7 @@ import url from 'node:url';
 import { logAnswer } from '../../utils/index.js';
 
 // Toggle this to use test or real data
-const USE_TEST_DATA = true;
+const USE_TEST_DATA = false;
 
 // Load data from files
 const THIS_FILENAME = url.fileURLToPath(import.meta.url);
@@ -27,9 +29,26 @@ const runOne = () => {
   const dataToUse = USE_TEST_DATA ? DATA.TEST1 : DATA.REAL;
   const lines = dataToUse.split('\n').filter((line) => line.trim().length > 0);
 
+  const firstCol: number[] = [];
+  const secondCol: number[] = [];
+  for (const nextLine of lines) {
+    const [first, second] = nextLine.split(/ +/u);
+    firstCol.push(Number.parseInt(first!, 10));
+    secondCol.push(Number.parseInt(second!, 10));
+  }
+
+  firstCol.sort((a, b) => a - b);
+  secondCol.sort((a, b) => a - b);
+
+  let total = 0;
+  // eslint-disable-next-line unicorn/no-for-loop
+  for (let index = 0; index < firstCol.length; index++) {
+    total += Math.abs(firstCol[index]! - secondCol[index]!);
+  }
+
   logAnswer({
-    answer: lines.length,
-    expected: USE_TEST_DATA ? undefined : undefined,
+    answer: total,
+    expected: USE_TEST_DATA ? 11 : undefined,
     partNum: 1,
     taskStartedAt,
   });
@@ -41,9 +60,26 @@ const runTwo = () => {
   const dataToUse = USE_TEST_DATA ? DATA.TEST2 : DATA.REAL;
   const lines = dataToUse.split('\n').filter((line) => line.trim().length > 0);
 
+  let similarity = 0;
+  const occurrenceCount = new Map<number, number>();
+
+  const firstCol: number[] = [];
+  for (const nextLine of lines) {
+    const [first, second] = nextLine.split(/ +/u);
+    firstCol.push(Number.parseInt(first!, 10));
+
+    const secondNumber = Number.parseInt(second!, 10);
+    const previousOccurrence = occurrenceCount.get(secondNumber) ?? 0;
+    occurrenceCount.set(secondNumber, previousOccurrence + 1);
+  }
+
+  for (const nextNumber of firstCol) {
+    similarity += nextNumber * (occurrenceCount.get(nextNumber) ?? 0);
+  }
+
   logAnswer({
-    answer: lines.length,
-    expected: USE_TEST_DATA ? undefined : undefined,
+    answer: similarity,
+    expected: USE_TEST_DATA ? 31 : undefined,
     partNum: 2,
     taskStartedAt,
   });
