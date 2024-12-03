ff963511a523d472adae9f176bc8f217a3972484
Martin Doyle <github@martindoyle.com>
2024-12-03 05:23:53 +0000

    AoC day three

--- src/puzzles/03/index.ts
+++ src/puzzles/03/index.ts
@@ -1,7 +1,7 @@
 import { getDataForPuzzle, logAnswer } from '../../utils/index.js';
 
 // Toggle this to use test or real data
-const USE_TEST_DATA = true;
+const USE_TEST_DATA = false;
 
 // Load data from files
 const data = getDataForPuzzle(import.meta.url);
@@ -11,10 +11,21 @@ const runOne = () => {
   const taskStartedAt = performance.now();
   const dataToUse = USE_TEST_DATA ? data.TEST1 : data.REAL;
   const lines = dataToUse.split('\n').filter((line) => line.trim().length > 0);
+  const regex = /mul\((\d+),(\d+)\)/gu;
+
+  let total = 0;
+  for (const line of lines) {
+    const matches = line.matchAll(regex);
+    for (const match of matches) {
+      const [, operand1, operand2] = match;
+      const result = Number.parseInt(operand1!, 10) * Number.parseInt(operand2!, 10);
+      total += result;
+    }
+  }
 
   logAnswer({
-    answer: lines.length,
-    expected: USE_TEST_DATA ? undefined : undefined,
+    answer: total,
+    expected: USE_TEST_DATA ? 161 : 161_289_189,
     partNum: 1,
     taskStartedAt,
   });
@@ -25,10 +36,28 @@ const runTwo = () => {
   const taskStartedAt = performance.now();
   const dataToUse = USE_TEST_DATA ? data.TEST2 : data.REAL;
   const lines = dataToUse.split('\n').filter((line) => line.trim().length > 0);
+  const regex = /mul\((\d+),(\d+)\)|do\(\)|don't\(\)/gu;
+
+  let total = 0;
+  let performMultiply = true;
+  for (const line of lines) {
+    const matches = line.matchAll(regex);
+    for (const match of matches) {
+      const [fullMatch, operand1, operand2] = match;
+      if (fullMatch.startsWith('don')) {
+        performMultiply = false;
+      } else if (fullMatch.startsWith('do')) {
+        performMultiply = true;
+      } else if (performMultiply) {
+        const result = Number.parseInt(operand1!, 10) * Number.parseInt(operand2!, 10);
+        total += result;
+      }
+    }
+  }
 
   logAnswer({
-    answer: lines.length,
-    expected: USE_TEST_DATA ? undefined : undefined,
+    answer: total,
+    expected: USE_TEST_DATA ? 48 : 83_595_109,
     partNum: 2,
     taskStartedAt,
   });
